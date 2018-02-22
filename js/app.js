33162b3f0dda211431d4ffb4ea930a82574fed2f
Emily Qinglu Zhang <emilyqlzhang@gmail.com>
2018-02-21 21:27:08 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -39,17 +39,12 @@ function initMap() {
 
     // call function, markerClickEven where a marker is clicked, an info window will pop out
     // and the marker will bounce
-    markerClickEvent(marker);
+    markerClickEvent(marker, infoWindow);
 
     // call function, markerMouseOut where the mouse moves away from a marker, the marker will
     // stop bouncing
     markerMouseOutEvent(marker);
   }
-
-  // filter out list based on input that is typed in
-  document.getElementById('go-filter').addEventListener('click', function() {
-    filterList();
-  });
 
   // create a ViewModel 
   var viewModel = function() {
@@ -61,12 +56,13 @@ function initMap() {
     // put each marker in array markers into observableArray stationList
     markers.forEach(function(stationItem) {
       self.stationList.push(stationItem);
-    });
+    });    
 
+    var infoWindow = new google.maps.InfoWindow();
+    
     // create an info windown when a station item in the list view is clicked
     this.populateInfoWindow = function(marker) {
-      var infoWindow = new google.maps.InfoWindow();
-    
+          
       // create URL that is for wikipedia API call
       var stationStr = marker.title.replace(/ /g, "+");
       var wikiUrl = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' +
@@ -92,24 +88,40 @@ function initMap() {
       
       // the maker will bounce when the corresponding station in list view is clicked
       marker.setAnimation(google.maps.Animation.BOUNCE);
+      setTimeout(function(){marker.setAnimation(null)}, 2 * 750);
       
       infoWindow.open(map, marker);
-      
-      // create an when the mouse is moved to marker, the marker will stop bouncing
-      marker.addListener('mouseover', function() {
-        this.setAnimation(null);
-      });   
     };
+
+    this.query = ko.observable("");
+
+    // filter the list based on user input
+    self.stationListFiltered = ko.computed(function() {
+      var query = self.query().toLowerCase();
+      if(!query) {
+        return self.stationList();
+      } else {
+        return ko.utils.arrayFilter(self.stationList(), function(station) {
+          return station.title.toLowerCase().startsWith(query);
+        });
+      }
+    });
+    
   };
 
-  ko.applyBindings(new viewModel());
+  ko.applyBindings(new viewModel()); 
 }
+
+// this function handles error related to Google Maps API
+function googleMapsError() {
+    alert("Sorry, not able to load the map");
+};
 
 // this function will populate an info window and make a marker bounce when
 // a marker is clicked
-function markerClickEvent(marker) {
+function markerClickEvent(marker, infowindow) {
   marker.addListener('click', function() {
-    populateInfoWindow(this);
+    populateInfoWindow(this, infowindow);
     this.setAnimation(google.maps.Animation.BOUNCE);
   });
 }
@@ -123,8 +135,8 @@ function markerMouseOutEvent(marker) {
 }
 
 // this function will populate the info window when a marker is clicked
-function populateInfoWindow(marker) {
-  var infoWindow = new google.maps.InfoWindow();
+function populateInfoWindow(marker, infowindow) {
+  //var infoWindow = new google.maps.InfoWindow();
   
   // create URL for wikipedia API call
   var stationStr = marker.title.replace(/ /g, "+");
@@ -138,7 +150,7 @@ function populateInfoWindow(marker) {
     success: function(response) {
       var stationName = response[1][0];
       var url = 'https://en.wikipedia.org/wiki/' + stationName;
-      infoWindow.setContent('<div>' + marker.title + '</div>' + '<br>' + '<a href="' + url + '">' + stationName + '</a>');            
+      infowindow.setContent('<div>' + marker.title + '</div>' + '<br>' + '<a href="' + url + '">' + stationName + '</a>');            
     },
     error: function(request, status, error) {
       if(request.status == 400) {
@@ -149,58 +161,5 @@ function populateInfoWindow(marker) {
     }
   });
 
-  infoWindow.open(map, marker);
-}
-
-// this function filter the list, show the station based on user's input 
-function filterList() {
-  var input, filter, ul, li, i, j;
-  
-  // get the input you type in
-  input = document.getElementById('stations-filter');
-  
-  // turn all characters of input into uppercase
-  filter = input.value.toUpperCase();
-  
-  // retrieve <ul> element with id "stations-list"
-  ul = document.getElementById('stations-list');
-  
-  // retrieve all <li> elements nested in the above <ul> element
-  li = ul.getElementsByTagName('li');
-
-  // loop through all <li> elements, show those match the search query,
-  // hide those that don't match the search query
-  for (i = 0; i < li.length; i++) {
-    
-    // get the station name of each <li> represents
-    stationName = li[i].innerHTML;
-    
-    // turn station name into uppercase
-    stationNameUpperCase = stationName.toUpperCase();
-    
-    // when user's input can be found matching the name of this station
-    if (stationNameUpperCase.indexOf(filter) > -1) {
-      
-      // keep <li> representing this station
-      li[i].style.display = '';
-      
-      // loop through all markers, show the marker representing this station
-      for (j = 0; j < markers.length; j++) {
-        if (stationName == markers[j].title) {
-          markers[j].setMap(map);
-        }
-      }                      
-    } else { // when user's input can't be found matching the name of this station
-      
-      // hide <li> representing this station
-      li[i].style.display = "none";
-      
-      // loop through all markers, hide the marker representing this station
-      for (j = 0; j < markers.length; j++) {
-        if (stationName == markers[j].title) {
-          markers[j].setMap(null);
-        }
-      }            
-    }
-  }
+  infowindow.open(map, marker);
 }
